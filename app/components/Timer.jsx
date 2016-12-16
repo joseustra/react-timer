ddc7e00fdfda08826ba1879de6d25902a0f94378
Jose Carlos Ustra Junior <dev@ustrajunior.com>
2016-12-16 14:48:28 -0300

    Add Timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,50 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
-  render: function() {
-    return <p>Timer.jsx</p>
-  }
+  getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'paused'
+    }
+  },
+  startTimer: function() {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused': 
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+	handleStatusChange: function(newStatus) {
+		this.setState({countdownStatus: newStatus});
+	},
+	render: function() {
+		var {count, countdownStatus} = this.state;
+		return (
+			<div>
+				<h1 className="page-title">Timer App</h1>
+				<Clock totalSeconds={count}/>
+				<Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange} />
+			</div>
+		)
+	}
 });
 
 module.exports = Timer;
